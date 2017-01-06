614fe52b40b186b699b8145ac9db9ec0c352487a
Roman Danko <mail@romandanko.sk>
2017-01-07 00:41:21 +0100

    Enhanced to accept job names, which includes spaces. Now spaces are transformed to underscore in manner of bacula/bareos director(which returns job name in var %n)

--- get-jobs.py
+++ get-jobs.py
@@ -11,12 +11,12 @@ encoding = locale.getdefaultlocale()[1]
 if conf['type'] == 'bareos':
     command="echo show jobs | timeout %d bconsole -c %s | grep -i name | cut -d \'\"\' -f 2" % (conf['bconsole_wait'], conf['bconsole_conf_file'])
 elif conf['type'] == 'bacula':
-    command="echo show jobs | timeout %d bconsole -c %s | awk '/^Job/ {sub(\"name=\",\"\",$2); print $2}'" % (conf['bconsole_wait'], conf['bconsole_conf_file'])
+    command="echo show jobs | timeout %d bconsole -c %s | grep \'Job: name=\' | sed -e \'s/Job\: name\=\(.*\) JobType.*/\\1/\'" % (conf['bconsole_wait'], conf['bconsole_conf_file'])
 else:
     command="echo"
 proc=subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 jobs_raw=proc.communicate()[0]
 
-result = dict( {'data': [ { "{#JOBNAME}": job } for job in jobs_raw.decode(encoding).split('\n') if job ]} )
+result = dict( {'data': [ { "{#JOBNAME}": job.replace(" ", "_") } for job in jobs_raw.decode(encoding).split('\n') if job ]} )
 
 print(json.JSONEncoder(sort_keys=True, indent=3).encode(result))
